61c26a3299f6e8b3cbd17be4f303c88a2503634f
Geoff White <geoffemw@keetonet.ca>
2021-11-05 19:15:00 -0400

    Initial setup

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
 createApp(App).use(router).mount("#app");
