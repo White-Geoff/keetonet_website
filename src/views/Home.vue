8b816d38579cbe7bc68f5095f105d78859909810
Geoff White <geoffemw@keetonet.ca>
2021-11-13 11:41:02 -0500

    add contact form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,112 @@
 <template>
-  <div class="home body h-screen align-bottom block">
-    <div class="h-2/4"></div>
-    <h1 class="h-8 font-bold block">Content goes here</h1>
-  </div>
+  <form ref="form" id="contact-form" @submit.prevent=Submit @reset=Reset>
+    <div class="z-10">
+      <h4>Name:</h4>
+      <input  name="name" placeholder="Enter name here…" 
+      class="form-control"  id="name-input"/>
+      <br/>
+      <h4>Phone:</h4>
+      <input name="phone"  placeholder="Enter phone number" 
+      class="form-control" id="phone-input"/>
+      <br/>
+      <h4>Email:</h4>
+      <input name="email"  placeholder="Enter email here…" 
+      class="form-control" id="email-input" />
+      <br/>
+      <h4>How can we help you?</h4>
+      <textarea name="message" rows="3" placeholder="Enter your message…" 
+      class="form-control" id="description-input"></textarea>
+      <br/>
+      <div>
+        <h4>Preferred Contact method:</h4>
+        <div class="flex">
+          <input name="contactChoice"  type="radio" id="contactChoiceEmail" class="form-control" value="email"/>
+          <label for="contactChoice">Email</label><br>
+          <input name="contactChoice"  type="radio" id="contactChoicePhone" class="form-control" value="phone"/>
+          <label for="contactChoice">Phone</label><br>
+        </div>
+      </div>
+      <br/>
+        <button type="reset"  id="resetbtn"
+        class="btn btn-lg">Reset</button>
+        <button type="submit"  value="Send"
+        class="btn btn-lg">Submit</button>
+    </div>
+    <div>
+        
+      </div>
+      <div class="z-20 -mt-72 h-40 mb-72">
+        <h4>I accept the terms</h4>
+        <input name="terms"  type="checkbox" id="acceptTerms" class="form-control"/>
+      </div>
+      
+</form>
 </template>
 
-// <script>
-// // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+<script>
+import emailjs from 'emailjs-com';
 
-// export default {
-//   name: "Home",
-//   components: {
-//     HelloWorld,
-//   },
-// };
-// </script>
+export default {
+  name: "ContactForm",
+  data () {
+      return {
+        name: "",
+      }
+    },
+  methods: {
+    Reset(){
+       this.name = ""
+       this.phone = ""
+       this.email = ""
+       this.message = ""
+       this.contactChoice = ""
+       this.terms = ""
+     },
+    Submit() {
+      emailjs.sendForm('service_67bbruu', 'template_6rji6sj', this.$refs.form, 'user_gdwqVVzFx51tCkVirTCP6')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
+    }
+     
+  }
+  };
 
-//<style scoped src="../index.css"></style>
+</script>
+
+<style>
+#contact-form {
+  margin: auto;
+  width: 90%;
+  max-width: 500px;
+  padding: 1rem 0%;
+}
+input {
+  height: 35px;
+}
+.form-control {
+  width: 100%;
+  text-align: center;
+  border-radius: 15px;
+  color: #000;
+  padding: 0.5rem;
+  margin: 0.5rem;
+}
+.btn {
+  background: #3A1FA5;
+  color: #FFFFFF;
+  padding: 0.5rem;
+  margin: 1rem;
+}
+#resetbtn {
+  background: #ff0000;
+}
+h4 {
+  padding: 0.2rem;
+}
+</style>
+
+
+
